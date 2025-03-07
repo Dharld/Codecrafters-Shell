210298822417c77000c973c7a44f3172b1c3b1dd
Yann <yanndjoumessi@gmail.com>
2025-03-07 14:50:29 -0500

    Wait for input user

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -10,6 +11,16 @@ int main(int argc, char *argv[]) {
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+  if (fgets(input, 100, stdin) != NULL) {
+    // Remove the new trailing new line
+    size_t len = strlen(input);
+    input[len - 1] = '\0';
+
+    printf("%s: command not found\n", input);
+  } else {
+    printf("Error reading input\n");
+  }; 
+
+
   return 0;
 }
